8f54ecba925a4551e880623ab1c9514e294aefb6
Alex Feinland <afein001@ucr.edu>
2014-02-08 18:12:56 -0800

    forked child process to exec command

--- my_shell.cpp
+++ my_shell.cpp
@@ -16,6 +16,9 @@
 // is ENTIRELY my own original work.
 #include <iostream>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 using namespace std;
 
@@ -39,7 +42,33 @@ void parse_input(int & argc, char *buf, char *argv[])
 }
 
 
+int execute_input(char *argv[])
+{
+    int fd = open(argv[1], O_CREAT, S_IRUSR | S_IWUSR); //open file for output
 
+    int pid = fork();
+
+    switch(pid)
+    {
+        case -1: //error forking
+            cerr << "Error running process" << endl;
+            exit(-1);
+            break;
+
+        case 0: //child
+            dup2(2, 1);
+            execvp(argv[0], argv);
+            cerr << "Error execvp program" << endl;
+            //exec program
+            break;
+
+        default: //parent
+        wait(pid);
+
+    }
+
+    return 0;
+}
 
 
 
@@ -51,13 +80,9 @@ int main()
     while(1)
     {
         print_prompt();
-        cin.getline(buf, 1023, '\n');
+        cin.getline(buf, 1023, '\n'); //get input
         parse_input(argc, buf, argv);
-        for(int i = 0; i < argc; ++i)
-            cout << argv[i] << endl;
-        //get input
-        //parse input
-        //execute on input
+        execute_input(argv); //execute on input
     }
 
 
